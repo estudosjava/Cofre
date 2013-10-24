7cf9623a91f8c8ec6c64bf4fdf38a9df80275be0
Gabriela Correa <gabriela01sc@gmail.com>
2013-10-24 17:59:26 -0200

    Metodo da classe UsuarioDao excluida para esta classe

--- src/main/java/br/com/cofrinho/dao/UsuarioDAOImpl.java
+++ src/main/java/br/com/cofrinho/dao/UsuarioDAOImpl.java
@@ -1,11 +1,16 @@
 package br.com.cofrinho.dao;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import br.com.cofrinho.ConnectionFactory;
 import br.com.cofrinho.model.Usuario;
 
 @Repository
@@ -48,4 +53,40 @@ public class UsuarioDAOImpl implements UsuarioDAO {
 	public List<Usuario> getUsuarios() {
 		return getCurrentSession().createQuery("from Usuario").list();
 	}
+	
+	public boolean validaUsuario(Usuario usuario) {
+
+		Connection con = new ConnectionFactory().getConnection();
+
+		ResultSet rs;
+
+		int iRegistros = 0;
+		boolean cadastrado = false;
+
+		String consulta = "select * from usuarios \n";
+		consulta += "where login = '" + usuario.getLogin() + "' \n";
+		consulta += "and senha = '" + usuario.getSenha() + "'";
+		
+		try {
+
+			Statement stmt = con.createStatement();
+			rs = stmt.executeQuery(consulta);
+
+			while (rs.next()) {
+				iRegistros++;
+			}
+			
+			if (iRegistros >= 1) {
+				cadastrado = true;
+			}
+
+			stmt.close();
+			con.close();
+
+		} catch (SQLException sqle) {
+			System.out.println("Erro no select: " + sqle);
+		}
+		return cadastrado;
+	}
+	
 }
