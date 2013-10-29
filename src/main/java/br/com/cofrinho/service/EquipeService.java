7ebbbd7d2340a8c5270fde187b94a2908da93032
Rafael Domingues <rafael.domingues@ilegra.com>
2013-10-29 09:04:07 -0200

    I delete the class interface of EquipeService

--- src/main/java/br/com/cofrinho/service/EquipeService.java
+++ src/main/java/br/com/cofrinho/service/EquipeService.java
@@ -2,14 +2,38 @@ package br.com.cofrinho.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import br.com.cofrinho.dao.EquipeDAO;
 import br.com.cofrinho.model.Equipe;
 
-public interface EquipeService {
+@Service
+@Transactional
+public class EquipeService {
 	
-	public void addEquipe(Equipe equipe);
-	public void updateEquipe(Equipe equipe);
-	public Equipe getEquipe(int codEquipe);
-	public void deleteEquipe(int codEquipe);
-	public List<Equipe> getEquipes();
+	@Autowired
+	private EquipeDAO equipeDAO;
+	
+	public void addEquipe(Equipe equipe) {
+		equipeDAO.addEquipe(equipe);		
+	}
+
+	public void updateEquipe(Equipe equipe) {
+		equipeDAO.updateEquipe(equipe);
+	}
+
+	public Equipe getEquipe(int codEquipe) {
+		return equipeDAO.getEquipe(codEquipe);
+	}
+
+	public void deleteEquipe(int codEquipe) {
+		equipeDAO.deleteEquipe(codEquipe);
+	}
+
+	public List<Equipe> getEquipes() {
+		return equipeDAO.getEquipes();
+	}
 
 }
