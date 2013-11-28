59c488a73aab79c6be510a4d39beb49b30b468f5
Rafael Domingues <rafael.domingues@ilegra.com>
2013-11-28 08:53:50 -0200

    Revert "Revert "I changed login and logout Options in the Cofrinho.""
    This reverts commit af47ccc921ea589db0436a92bb25cbdf5f7641ee.

--- src/main/java/br/com/cofrinho/controller/LoginController.java
+++ src/main/java/br/com/cofrinho/controller/LoginController.java
@@ -1,12 +1,11 @@
 package br.com.cofrinho.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.com.cofrinho.model.User;
@@ -14,6 +13,7 @@ import br.com.cofrinho.service.UserService;
 import br.com.cofrinho.utils.LoadDefaultMessage;
 
 @Controller
+@SessionAttributes("userlog")
 @RequestMapping(value="/login")
 public class LoginController {
 	@Autowired
@@ -21,10 +21,10 @@ public class LoginController {
 	
 	@RequestMapping(value="/validaUsuario", method=RequestMethod.POST)
 	public ModelAndView validateUser(@ModelAttribute User user){
-		List<User> list = userService.checkUser(user); 
-		if(userService.checkUser(user).size() > 0){
+		User userReturn  = userService.checkUser(user); 
+		if(userReturn!=null){
 			ModelAndView modelAndView = new ModelAndView("homepage");
-			modelAndView.addObject("userlog", list);
+			modelAndView.addObject("userlog", userReturn);
 			return modelAndView;
 		}else{
 			ModelAndView modelAndView = new ModelAndView("login");
@@ -32,5 +32,13 @@ public class LoginController {
 			modelAndView.addObject("message",loadDefaultMessage.getMessage("register.failed"));
 			return modelAndView;
 		}
-	}	
+	}
+	
+	@RequestMapping(value="/finalizaUsuario", method=RequestMethod.GET)
+	public ModelAndView exitUser(@ModelAttribute User user){
+			User userReturn = null;
+			ModelAndView modelAndView = new ModelAndView("login");
+			System.out.println(userReturn);
+			return modelAndView;
+		}
 }
--- src/main/webapp/WEB-INF/views/login.jsp
+++ src/main/webapp/WEB-INF/views/login.jsp
@@ -4,7 +4,7 @@
 <html lang="en">
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Erro ao efetuar Login</title>
+<title>Login</title>
 
 	<link href="/Cofre/resources/css/bootstrap.css" rel="stylesheet">
 	<link href="/Cofre/resources/css/bootstrap-responsive.css" rel="stylesheet">
